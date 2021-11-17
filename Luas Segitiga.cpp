ae91e225017e8b8e31a683b4b4ff9c73791dd05c
MRadityaAdhirajasa <92293185+MRadityaAdhirajasa@users.noreply.github.com>
2021-11-17 23:42:19 +0700

    Add files via upload

---
+++ Luas Segitiga.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     double jarijari, luas;
+
+     float p = 3.14;
+
+     cout<<"Masukan Jari-Jari Lingkaran = ";cin>>jarijari;
+     luas = p * (jarijari*jarijari);
+     cout<<"Luas Lingkaran = 3,14 X "<<jarijari<<" X "<<jarijari<<" = "<<luas;
+
+     return 0;
+}
