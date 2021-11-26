79e0e589cf57e8544223b825e2898561ef1fdd05
MRadityaAdhirajasa <92293185+MRadityaAdhirajasa@users.noreply.github.com>
2021-11-26 22:58:09 +0700

    Add files via upload

---
+++ Teks Bergerak.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h>
+
+using namespace std;
+
+void animasi (char kata[100])
+{
+	for(int i=0; i<strlen(kata); i++)
+	{
+		cout<<kata[i];
+		Sleep(100);
+	}
+}
+
+int main()
+	{
+
+		
+		system("Color B5");
+		animasi("MORIOH ");
+		Sleep(1000);
+		
+		system("Color E4");
+		cout<<"CHO ";
+		Sleep(1000);
+		
+		system("Color 2E");
+		animasi("RADIO");
+		system("cls");
+		Sleep(500);
+
+	int x; double y;
+   	char a[]="\t MORIOH CHO RADIO";
+   	for(x=0; a[x]!=NULL; x++)
+   {
+		cout<<a[x];
+      	for(y=0; y<=9999999; y++){
+      }
+   }
+   getch();
+   system("cls");
+   
+    char b[]="\t\t MORIOH CHO RADIO";
+   	for(x=0; b[x]!=NULL; x++)
+   {
+		cout<<b[x];
+      	for(y=0; y<=9999999; y++){
+      }
+   }
+   getch();
+   system("cls");
+   
+    char c[]="\t\t\t MORIOH CHO RADIO";
+   	for(x=0; c[x]!=NULL; x++)
+   {
+		cout<<c[x];
+      	for(y=0; y<=9999999; y++){
+      }
+   }
+   getch();
+   system("cls");
+
+
+
+   
+   
+   
+   	
+   	printf("\n");
+   	for(x=0; c[x]!=NULL; x++)
+   {
+		printf("%c",c[x]);
+      	for(y=0; y<=9999999; y++){
+      }
+   }
+   getch();
+   system("cls");
+
+
+   	printf("\n\n");
+   	for(x=0; c[x]!=NULL; x++)
+   {
+		printf("%c",c[x]);
+      	for(y=0; y<=9999999; y++){
+      }
+   }
+   getch();
+   system("cls");
+     
+        	printf("\n\n\n");
+   	for(x=0; c[x]!=NULL; x++)
+   {
+		printf("%c",c[x]);
+      	for(y=0; y<=9999999; y++){
+      }
+   }
+   getch();
+
+	}
+
